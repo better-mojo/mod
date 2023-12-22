edfd84e4dc91d39a91dcb3c02d21ef0fc9310b23
Henry.Huang <hhglory@outlook.com>
2023-12-22 18:26:24 +0800

    update

--- packages/mod-cli/src/mod_cli/main.py
+++ packages/mod-cli/src/mod_cli/main.py
@@ -1,6 +1,9 @@
 import typer
+from typing import Optional, Tuple
+from pathlib import Path
 from typing_extensions import Annotated
 from loguru import logger
+import os
 
 from enum import Enum
 
@@ -24,16 +27,49 @@ class NewType(str, Enum):
     lib = "lib"
 
 
-@app.command(help="Create a new project: --bin, --lib, --app")
+def is_valid_directory(path):
+    import re
+
+    if isinstance(path, Path):
+        path = str(path)
+
+    pattern = r'^([\w.-]+)+/?$'
+    return re.match(pattern, path) is not None
+
+
+@app.command(help="Create a new project: [--bin, --lib]")
 def new(
-    project_name: str,
-    typ: Annotated[NewType, typer.Option("--type", help="The type of project")] = NewType.bin,
+    project_name: Annotated[Path, typer.Argument(metavar="path", help="The path to create the project at.")],
+    is_bin: Annotated[bool, typer.Option("--bin", help="Create a binary project")] = False,
+    is_lib: Annotated[bool, typer.Option("--lib", help="Create a library project")] = False,
+    # typ: Annotated[NewType, typer.Option("--type", help="The type of project")] = NewType.bin,
 ):
     """
     New
     """
     typer.echo("New a mojo project")
-    logger.info(f"project_name: {project_name}, project_type: {typ}")
+    logger.info(f"project name: {project_name}")
+    logger.info(f"project type: is_bin: {is_bin}, is_lib: {is_lib}")
+
+    if is_valid_directory(project_name):
+        print(f"{project_name} is not a directory")
+        # raise typer.Exit(1)
+        raise typer.Abort(f"{project_name} is not a directory")
+
+    if os.path.exists(project_name):
+        typer.confirm(f"❗️{project_name} already exists, do you want to overwrite it?", abort=True)
+        print(f"\n❗️Overwriting {project_name}")
+
+    try:
+        os.makedirs(project_name, exist_ok=True)  # allow to overwrite
+    except Exception as e:
+        print(e)
+        # raise typer.Exit(1)
+        raise typer.Abort(e)
+
+    # 创建路径
+
+    # 判断 project_name 是不是路径(相对路径和绝对路径)
 
 
 @app.command()
