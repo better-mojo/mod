39697c70517b2a94abce52b978daa1f89e3a9a09
Henry.Huang <hhglory@outlook.com>
2023-12-22 20:52:35 +0800

    update

--- packages/mod-cli/src/mod_cli/core/new/layout.py
+++ packages/mod-cli/src/mod_cli/core/new/layout.py
@@ -3,6 +3,22 @@ from jinja2 import Environment
 from jinja2.loaders import FileSystemLoader
 
 
+class ProjectLayout:
+    pass
+
+    def __init__(self):
+        self.f_mod = None
+        self.f_readme = None
+        self.f_lib = None
+        self.f_bin = None
+
+    def load_template(self):
+        pass
+
+    def write(self):
+        pass
+
+
 def read_template():
     # 支持相对路径:
     env = Environment(loader=FileSystemLoader("../../templates"))
@@ -10,6 +26,7 @@ def read_template():
     tmpl = env.get_template("mod.toml.jinja2")
 
     logger.info(f"{tmpl.render()}")
+    logger.info(f"{env.get_template('layout_bin/src/main.mojo').render()}")
 
 
 def test():
